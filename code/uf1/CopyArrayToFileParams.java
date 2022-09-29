87d577dbe6e34e70dea3e71628c027aeed4dbccc
Rusben <rusben@gmail.com>
2022-09-29 18:49:16 +0200

    CopyArrayToFile

---
+++ code/uf1/CopyArrayToFileParams.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class CopyArrayToFile {
+
+    public static void main(String[] args) {
+
+        String[] words = new String[args.length - 1];
+        for (int i = 0; i < args.length - 1; i++) {
+            words[i] = args[i];
+        }
+
+        File f = new File(args[args.length - 1]);
+
+        saveWordsIntoFile(words, f);
+        showWords(f);
+    }
+
+    private static void showWords(File f) {
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(f));
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void saveWordsIntoFile(String[] words, File f) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(f));
+            for (int i = 0; i < words.length; i++) {
+                bw.write(words[i]);
+                bw.newLine();
+            }
+            bw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
